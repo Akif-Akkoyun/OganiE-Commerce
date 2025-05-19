20403d4d2cee1f9e8746b5b62c06a6b0cb2b5bc7
Akif Akkoyun <144811507+Akif-Akkoyun@users.noreply.github.com>
2025-05-19 03:50:39 +0300

    Auth layer dbContext connection
    DbContext connection was written for the auth layer and it is taken to the Persistence layer via extensions. Entities were not written. The design may change, I can proceed directly through a single API. That's why I did not write entities.

--- Presentation/Authentication.Api/Program.cs
+++ Presentation/Authentication.Api/Program.cs
@@ -1,14 +1,15 @@
+using Authentication.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddAuthExtensions(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
